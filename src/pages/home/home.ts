2583bfe6cb7726b798a8cd6275597fc04144a319
Leo Placencia <lplacencia@moldeable.com>
2018-05-10 11:52:10 -0300

    Aplicación con geo

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Geolocation, Geoposition } from '@ionic-native/geolocation';
+
+declare var google;
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,64 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  map: any;
 
+  constructor(public navCtrl: NavController, public geolocation: Geolocation) {
+
+  }
+  ionViewDidLoad(){
+    this.getPosition();
+  }
+  getPosition():any{
+    this.geolocation.getCurrentPosition().then(response => {
+      this.loadMap(response);
+    })
+    .catch(error =>{
+      console.log(error);
+    })
+  }
+
+  loadMap(position: Geoposition){
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+    console.log(latitude, longitude);
+    
+    // create a new map by passing HTMLElement
+    let mapEle: HTMLElement = document.getElementById('map');
+  
+    // create LatLng object
+    let myLatLng = {lat: latitude, lng: longitude};
+  
+    // create map
+    this.map = new google.maps.Map(mapEle, {
+      center: myLatLng,
+      zoom: 12
+    });
+    // // Mensaje
+    // var infoWindows = new google.maps.InfoWindows({
+    //   content: 'Aquí estoy!!!'
+    // });
+    // // Mostramos el marcador en un mapa(this.map)
+    // let marker = new google.maps.Marker({
+    //   position: myLatLng,
+    //   map: this.map,
+    // });
+    // // muestra el mensaje cuando seleccionas el marcador
+    // marker.addListener('click', function(){
+    //   infoWindows.open(this.map, marker);
+    // });
+    // google.maps.event.addListenerOnce(this.map, 'idle', () => {
+      
+    //   mapEle.classList.add('show-map');
+    // });
+    google.maps.event.addListenerOnce(this.map, 'idle', () => {
+      let marker = new google.maps.Marker({
+        position: myLatLng,
+        map: this.map,
+        title: 'Hello World!'
+      });
+      mapEle.classList.add('show-map');
+    });
   }
 
 }
